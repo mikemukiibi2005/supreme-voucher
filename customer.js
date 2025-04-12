bddf6e19de3a81f2a6914054af41c31f0d39e198
Michael Mukiibi <mikemukiibi2005@gmail.com>
2025-04-12 19:40:21 +0300

    Added customer setup

---
+++ customer.js
@@ -0,0 +1,10 @@
+// Customer placing the order
+export class Customer {
+    constructor(name, phone) {
+        // Customer name or identifier
+        this.name = name;
+
+        // Customer phone
+        this.phone = phone;
+    }
+}
