f493d06770890bf3ef6429b79a688dbd585d1de6
Michael Mukiibi <mikemukiibi2005@gmail.com>
2025-04-13 17:39:39 +0300

    Removed customer from voucher

--- voucher.js
+++ voucher.js
@@ -1,9 +1,6 @@
 // Voucher for a service
 export class Voucher {
     constructor(customer, service, discount) {
-        // Customer redeeming the voucher
-        this.customer = customer;
-
         // Service covered by voucher
         this.service = service;
 
